191ec5a7512e5ea7a4ac2ab6d28cea47601c7548
Muhammad Rifky Abimayu <34646726+rifkytech@users.noreply.github.com>
2018-08-19 16:12:02 +0700

    Penambahan Variable & cURL

--- includes/proccess.php
+++ includes/proccess.php
@@ -5,3 +5,20 @@
 * Last Update : 19 Agustus 2018
 *
 **/
+
+/** Variable Yang Dibutuhkan **/
+$api_key    = "YOURAPIKEY"; // Your User API Key (Mohon Jangan Ada Karakter Spasi !)
+$service    = $_POST['service']; // Service, Available A, NS, CNAME & MX
+$domain     = $_POST['domain']; // Domain Name
+$subdomain  = $_POST['subdomain']; // Subdomain Name to Create
+$content    = $_POST['content']; // Example Content IP or Domain Address
+
+/** cURL Prosess **/
+$ch         = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://panel.hostddns.us/api/v1/order/?key=$api_key&service=$service&domain=$domain&subdomain=$subdomain&content=$content");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+$result     = curl_exec($ch);
+echo $result;
+?>
