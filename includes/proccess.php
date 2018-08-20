9e372d60b993d97f8c696bace2c08f6fb09914dd
Muhammad Rifky Abimayu <34646726+rifkytech@users.noreply.github.com>
2018-08-20 07:54:24 +0700

    Edit API Url

--- includes/proccess.php
+++ includes/proccess.php
@@ -2,20 +2,22 @@
 /**
 *
 * Engine Proses Dengan PHP cURL
-* Last Update : 19 Agustus 2018
+* Last Update : 20 Agustus 2018
 *
 **/
 
 /** Variable Yang Dibutuhkan **/
+$api_url    = "https://panel.hostddns.us/api/v1/order/"; // Stable API Url v1 
 $api_key    = "YOUR API KEY"; // Your User API Key (Mohon Jangan Ada Karakter Spasi !)
 $service    = $_POST['service']; // Service, Available A, NS, CNAME & MX
 $domain     = $_POST['domain']; // Domain Name
 $subdomain  = $_POST['subdomain']; // Subdomain Name to Create
 $content    = $_POST['content']; // Example Content IP or Domain Address
+/** End Variable **/
 
 /** cURL Prosess **/
 $ch         = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://panel.hostddns.us/api/v1/order/?key=$api_key&service=$service&domain=$domain&subdomain=$subdomain&content=$content");
+curl_setopt($ch, CURLOPT_URL, "$api_url?key=$api_key&service=$service&domain=$domain&subdomain=$subdomain&content=$content");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_VERBOSE, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -24,11 +26,9 @@ $newarray = json_decode(trim($result), TRUE);
 $status = $newarray["success"];
 $message = $newarray["message"];
 if($status == 'false'){
-	echo "Error, Unable To Create Subdomain";
 	echo "<pre>Message : " .$message."</pre>";
 } else {
 	echo "<pre>" .$result. "</pre>";
 }
-
-
+/** End cURL Prosess **/
 ?>
